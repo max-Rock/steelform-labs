10f4b82c4e398760936346ea8198b0260b6bc1fd
maxrock612 <maxrock612@gmail.com>
2026-01-06 14:45:52 +0000

    minor changes 5

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'build' ? '/steelform-labs/' : '/';
-  return {
-    plugins: [react()],
-    base: base,
-    server: {
-      hmr: {
-        overlay: false,
-      },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false,
     },
-  }
+  },
 })
